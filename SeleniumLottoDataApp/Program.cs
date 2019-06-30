de1e3a47209155e50b281332ce22683828dacf86
henryma14 <henryma14@gmail.com>
2019-06-29 23:05:44 -0700

    Fixed a bug in LottoMegaMillions and package libs reference changes.

--- SeleniumLottoDataApp/Program.cs
+++ SeleniumLottoDataApp/Program.cs
@@ -11,68 +11,68 @@ namespace SeleniumLottoDataApp
     {
         static void Main(string[] args)
         {
-            LottoBase obj = new LottoBC49();
-            obj.InsertDb();
+            //LottoBase obj = new LottoBC49();
+            //obj.InsertDb();
 
-            obj = new Lotto649();
-            obj.InsertDb();
+            //obj = new Lotto649();
+            //obj.InsertDb();
 
-            obj = new LottoMAX();
-            obj.InsertDb();
+            //obj = new LottoMAX();
+            //obj.InsertDb();
 
-            obj = new LottoCaSuperlottoPlus();
-            obj.InsertDb();
-
-
-
-            obj = new LottoEuroJackpot();
-            obj.InsertDb();
-
-            obj = new LottoEuroMillions();
-            obj.InsertDb();
-
-            obj = new LottoFloridaFantasy5();
-            obj.InsertDb();
-
-            obj = new LottoFloridaLotto();
-            obj.InsertDb();
-
-            obj = new LottoFloridaLucky();
-            obj.InsertDb();
-
-            obj = new LottoColorado();
-            obj.InsertDb();           
-
-            obj = new LottoNYLotto();
-            obj.InsertDb();
+            //obj = new LottoCaSuperlottoPlus();
+            //obj.InsertDb();
 
 
 
-            obj = new LottoSuperLotto();
-            obj.InsertDb();
+            //obj = new LottoEuroJackpot();
+            //obj.InsertDb();
 
-            obj = new LottoSevenLotto();
-            obj.InsertDb();
+            //obj = new LottoEuroMillions();
+            //obj.InsertDb();
 
-            obj = new LottoSSQ();
-            obj.InsertDb();
+            //obj = new LottoFloridaFantasy5();
+            //obj.InsertDb();
 
-            obj = new LottoNewYorkTake5();
-            obj.InsertDb();
+            //obj = new LottoFloridaLotto();
+            //obj.InsertDb();
 
-            obj = new LottoTexasCashFive();
-            obj.InsertDb();
+            //obj = new LottoFloridaLucky();
+            //obj.InsertDb();
 
-            obj = new LottoGermanLotto();
-            obj.InsertDb();
+            //obj = new LottoColorado();
+            //obj.InsertDb();           
 
-            obj = new LottoConnecticutLotto();
-            obj.InsertDb();
+            //obj = new LottoNYLotto();
+            //obj.InsertDb();
 
-            obj = new LottoPowerBall();
-            obj.InsertDb();
 
-            obj = new LottoMegaMillions();
+
+            //obj = new LottoSuperLotto();
+            //obj.InsertDb();
+
+            //obj = new LottoSevenLotto();
+            //obj.InsertDb();
+
+            //obj = new LottoSSQ();
+            //obj.InsertDb();
+
+            //obj = new LottoNewYorkTake5();
+            //obj.InsertDb();
+
+            //obj = new LottoTexasCashFive();
+            //obj.InsertDb();
+
+            //obj = new LottoGermanLotto();
+            //obj.InsertDb();
+
+            //obj = new LottoConnecticutLotto();
+            //obj.InsertDb();
+
+            //obj = new LottoPowerBall();
+            //obj.InsertDb();
+
+            LottoBase obj = new LottoMegaMillions();
             obj.InsertDb();
         }
     }
