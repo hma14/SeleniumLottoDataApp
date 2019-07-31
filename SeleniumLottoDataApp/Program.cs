c0ffdc86deb3fada0e973d3d7d7e97f74aeb884d
Henry Ma <henryma14@gmail.com>
2019-07-31 10:51:39 -0700

    Minor

--- SeleniumLottoDataApp/Program.cs
+++ SeleniumLottoDataApp/Program.cs
@@ -12,71 +12,71 @@ namespace SeleniumLottoDataApp
         static void Main(string[] args)
         {
 
-            //LottoBase obj = new LottoBC49();
-            //obj.InsertDb();
+            LottoBase obj = new LottoBC49();
+            obj.InsertDb();
 
-            //obj = new Lotto649();
-            //obj.InsertDb();
+            obj = new Lotto649();
+            obj.InsertDb();
 
-            //obj = new LottoMAX();
-            //obj.InsertDb();
+            obj = new LottoMAX();
+            obj.InsertDb();
 
-            //obj = new LottoCaSuperlottoPlus();
-            //obj.InsertDb();
-
-
-
-            //obj = new LottoEuroJackpot();
-            //obj.InsertDb();
-
-            //obj = new LottoEuroMillions();
-            //obj.InsertDb();
-
-            //obj = new LottoFloridaFantasy5();
-            //obj.InsertDb();
-
-            //obj = new LottoFloridaLotto();
-            //obj.InsertDb();
-
-            //obj = new LottoFloridaLucky();
-            //obj.InsertDb();
-
-            //obj = new LottoColorado();
-            //obj.InsertDb();
-
-            //obj = new LottoNYLotto();
-            //obj.InsertDb();
+            obj = new LottoCaSuperlottoPlus();
+            obj.InsertDb();
 
 
 
-            //obj = new LottoSuperLotto();
-            //obj.InsertDb();
+            obj = new LottoEuroJackpot();
+            obj.InsertDb();
 
-            //obj = new LottoSevenLotto();
-            //obj.InsertDb();
+            obj = new LottoEuroMillions();
+            obj.InsertDb();
 
-            //obj = new LottoSSQ();
-            //obj.InsertDb();
+            obj = new LottoFloridaFantasy5();
+            obj.InsertDb();
 
-            //obj = new LottoNewYorkTake5();
-            //obj.InsertDb();
+            obj = new LottoFloridaLotto();
+            obj.InsertDb();
 
-            //obj = new LottoTexasCashFive();
-            //obj.InsertDb();
+            obj = new LottoFloridaLucky();
+            obj.InsertDb();
 
-            //obj = new LottoGermanLotto();
-            //obj.InsertDb();
+            obj = new LottoColorado();
+            obj.InsertDb();
 
-            //obj = new LottoConnecticutLotto();
-            //obj.InsertDb();
+            obj = new LottoNYLotto();
+            obj.InsertDb();
 
-            //obj = new LottoPowerBall();
-            //obj.InsertDb();
 
-            //obj = new LottoMegaMillions();
-            //obj.InsertDb();
 
-            LottoBase  obj = new LottoDailyGrand();
+            obj = new LottoSuperLotto();
+            obj.InsertDb();
+
+            obj = new LottoSevenLotto();
+            obj.InsertDb();
+
+            obj = new LottoSSQ();
+            obj.InsertDb();
+
+            obj = new LottoNewYorkTake5();
+            obj.InsertDb();
+
+            obj = new LottoTexasCashFive();
+            obj.InsertDb();
+
+            obj = new LottoGermanLotto();
+            obj.InsertDb();
+
+            obj = new LottoConnecticutLotto();
+            obj.InsertDb();
+
+            obj = new LottoPowerBall();
+            obj.InsertDb();
+
+            obj = new LottoMegaMillions();
+            obj.InsertDb();
+
+            obj = new LottoDailyGrand();
             obj.InsertDb();
 
             obj.CloseDriver();
